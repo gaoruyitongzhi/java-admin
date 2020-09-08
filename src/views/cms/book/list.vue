f19ae59ebd1d34882c240c8fe6efb2c70f1356f0
zhangpeng_global <zhangpengaly@163.com>
2020-09-08 08:49:44 +0800

    添加了根据作者、是否连载、书籍状态、是否原创来查询书籍功能；完善连载和状态属性名与后端一致

--- src/views/cms/book/list.vue
+++ src/views/cms/book/list.vue
@@ -2,8 +2,41 @@
   <div id="app-container">
     <!-- 查询表单 -->
     <el-form :inline="true" class="demo-form-inline">
-      <el-form-item>
+      <el-form-item  label="书名">
         <el-input v-model="bookQuery.title" placeholder="书名" />
+      </el-form-item>
+      <el-form-item label="作者">
+        <el-input v-model="bookQuery.author" placeholder="作者" />
+      </el-form-item>
+      <el-form-item label="连载">
+        <el-select v-model="bookQuery.serialize" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="状态">
+        <el-select v-model="bookQuery.status" placeholder="请选择">
+          <el-option
+            v-for="item in options2"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="原创">
+        <el-select v-model="bookQuery.original" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="授权开始时间">
         <el-date-picker
@@ -43,13 +76,13 @@
       <el-table-column prop="secondary" label="二级分类" />
       <el-table-column label="连载">
         <template slot-scope="scope">
-          {{ scope.row.serial === 1 ? '是' : '否'}}
+          {{ scope.row.serialize === 1 ? '是' : '否'}}
         </template>
       </el-table-column>
       <el-table-column prop="workNumber" label="字数" />
       <el-table-column label="状态">
         <template slot-scope="scope">
-          {{ scope.row.state === 1 ? '上线' : '未上线'}}
+          {{ scope.row.status === 1 ? '上线' : '未上线'}}
         </template>
       </el-table-column>
       <el-table-column label="全本收费">
@@ -71,11 +104,11 @@
       </el-table-column>
       <el-table-column label="操作" align="center">
         <template slot-scope="scope">
-          <el-button type="danger" size="mini" @click="deleteBook(scope.row.id, scope.row.state)">删除</el-button>
+          <el-button type="danger" size="mini" @click="deleteBook(scope.row.id, scope.row.status)">删除</el-button>
           <router-link :to="'/cms/book/update/'+scope.row.id">
             <el-button type="primary" size="mini" >修改</el-button>
           </router-link>
-          <el-button v-if="scope.row.state === 0" type="danger" size="mini" icon="el-icon-delete" @click="upOrDownBook(scope.row.id, 1)">上架书籍</el-button>
+          <el-button v-if="scope.row.status === 0" type="danger" size="mini" icon="el-icon-delete" @click="upOrDownBook(scope.row.id, 1)">上架书籍</el-button>
           <el-button v-else type="danger" size="mini" @click="upOrDownBook(scope.row.id, 0)">下架书籍</el-button>
         </template>
       </el-table-column>
@@ -104,7 +137,22 @@
         total:0,//总记录数
         pageNo:1,//页码
         pageSize:5,// 每页显示条数
-        currentTime:''//当前时间
+        currentTime:'',//当前时间
+        options: [{
+          value: '1',
+          label: '是'
+        }, {
+          value: '0',
+          label: '否'
+        }],
+        options2: [{
+          value: '1',
+          label: '上线'
+        }, {
+          value: '0',
+          label: '未上线'
+        }],
+        value: ''
       }
     },
     created() {//在页面渲染之前，一般先调用methods定义的方法
@@ -149,13 +197,13 @@
             })
         })
       },
-      upOrDownBook(bookId, state) { // ## 设置书籍上下架
+      upOrDownBook(bookId, status) { // ## 设置书籍上下架
         this.$confirm('此操作将改变书籍上下线记录, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
-          book.upOrDownBook(bookId, state)
+          book.upOrDownBook(bookId, status)
             .then(response => {
               this.$message({
                 type: 'success',
